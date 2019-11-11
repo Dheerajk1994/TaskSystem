d0eff2987ca4716806dae9c0852d2132793db635
Dheerajkumar Haritheesan <dheerajk.har@gmail.com>
2019-11-10 20:35:39 -0800

    Add files via upload

---
+++ Task.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Task
+{
+    public string taskName { get; private set; }
+    public bool isDone { get; protected set; }
+    public GameObject entity;
+    
+    public event Action TaskCompleted = delegate { };
+
+    public Task(string taskName, GameObject entity)
+    {
+        this.taskName = taskName;
+        this.entity = entity;
+    }
+
+    public abstract bool IsValidated();
+    public abstract void Validate(uint workAmount);
+    public abstract void Execute(uint workAmount);
+
+    protected virtual void OnFinish()
+    {
+        isDone = true;
+        TaskCompleted();
+    }
+}
